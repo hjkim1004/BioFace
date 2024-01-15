9e7f87a5080bbe8aec85dd9b83323a3a34d857e3
hjkim1004 <hjkim1004@users.noreply.github.com>
2024-01-15 17:07:02 +0000

    deploy: 1b8d26b8125d96957982e19368aeef5b8197600d

---
+++ static/js/238.93c04d2a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmedia_app=self.webpackChunkmedia_app||[]).push([[238],{2238:(e,s,n)=>{n.r(s),n.d(s,{default:()=>p});var a=n(4306),r=n(2791),c=n(1490),d=n(184);const t=()=>(0,d.jsxs)(a.YI,{children:[(0,d.jsx)(a.WD,{name:"App",children:(0,d.jsx)(a.wA,{name:"Loader",children:(0,d.jsx)(a.$e,{children:(0,d.jsx)(i,{})})})}),(0,d.jsx)(c.Z,{})]});function i(){return(0,d.jsx)(r.Fragment,{children:"Loading..."})}const p=()=>(0,d.jsx)(a.Vt,{palette:(0,d.jsx)(t,{})})}}]);
+//# sourceMappingURL=238.93c04d2a.chunk.js.map
